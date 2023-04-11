0c40c21ca4be120733c030e68409df7e6b991767
Taiyo Scanlon-Kimura <taiyo.scanlon-kimura@fulbrightmail.org>
2023-04-10 21:00:09 -0700

    completed melon_info.py, made corrections to melons.py

--- melons.py
+++ melons.py
@@ -1,42 +1,42 @@
-melons_list = {
+melons_dict = {
     'Honeydew' : {
         'price': 0.99,
         'seedless': True,
-        'flesh_color': None
-        'rind_color': None
-        'average_weight': None
+        'flesh_color': None,
+        'rind_color': None,
+        'average_weight': None,
     },
     
     'Crenshaw' : {
         'price': 2.00,
         'seedless': False,
-        'flesh_color': None
-        'rind_color': None
-        'average_weight': None
+        'flesh_color': None,
+        'rind_color': None,
+        'average_weight': None,
     },
     
     'Crane' : {
         'price': 2.50,
         'seedless': False,
-        'flesh_color': None
-        'rind_color': None
-        'average_weight': None
+        'flesh_color': None,
+        'rind_color': None,
+        'average_weight': None,
     },
     
     'Casaba' : {
-        'price': 2.50
+        'price': 2.50,
         'seedless': False,
-        'flesh_color': None
-        'rind_color': None
-        'average_weight': None
+        'flesh_color': None,
+        'rind_color': None,
+        'average_weight': None,
     },
     
     'Cantaloupe' : {
         'price': 0.99,
         'seedless': False,
-        'flesh_color': None
-        'rind_color': None
-        'average_weight': None
+        'flesh_color': None,
+        'rind_color': None,
+        'average_weight': None,
     }
 }
 
@@ -50,9 +50,6 @@ melons_list = {
 # melon_dict = {1 : price, seedless, flesh_color, rind_color, average_average weight}
 # first use dict.items on all three dicts?
 
-names_list = melon_names.items()
-prices_list = melon_prices.items()
-seeds_list = melon_seedlessness.items()
-
-for instance in 
-master_list = (f"{names_list[instance]}, {prices_list[instance]}, {seeds_list[instance]}")
+# names_list = melon_names.items()
+# prices_list = melon_prices.items()
+# seeds_list = melon_seedlessness.items()
