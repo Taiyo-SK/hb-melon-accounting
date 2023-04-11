f945a7a852a4076f35a5f6b1c8e40474914cc53f
Taiyo Scanlon-Kimura <taiyo.scanlon-kimura@fulbrightmail.org>
2023-04-10 20:42:04 -0700

    refactored melons.py

---
+++ melons.py
@@ -0,0 +1,58 @@
+melons_list = {
+    'Honeydew' : {
+        'price': 0.99,
+        'seedless': True,
+        'flesh_color': None
+        'rind_color': None
+        'average_weight': None
+    },
+    
+    'Crenshaw' : {
+        'price': 2.00,
+        'seedless': False,
+        'flesh_color': None
+        'rind_color': None
+        'average_weight': None
+    },
+    
+    'Crane' : {
+        'price': 2.50,
+        'seedless': False,
+        'flesh_color': None
+        'rind_color': None
+        'average_weight': None
+    },
+    
+    'Casaba' : {
+        'price': 2.50
+        'seedless': False,
+        'flesh_color': None
+        'rind_color': None
+        'average_weight': None
+    },
+    
+    'Cantaloupe' : {
+        'price': 0.99,
+        'seedless': False,
+        'flesh_color': None
+        'rind_color': None
+        'average_weight': None
+    }
+}
+
+# adding flesh color, rind color, and average weight
+# price float
+# seedlessness Bool
+# flesh_color None
+# rind_color None
+# average_weight None
+
+# melon_dict = {1 : price, seedless, flesh_color, rind_color, average_average weight}
+# first use dict.items on all three dicts?
+
+names_list = melon_names.items()
+prices_list = melon_prices.items()
+seeds_list = melon_seedlessness.items()
+
+for instance in 
+master_list = (f"{names_list[instance]}, {prices_list[instance]}, {seeds_list[instance]}")
